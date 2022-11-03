8ed1e62dd44c071cc8b1fbe38d1a0ea31cb41a38
ChenghaoLi <513322150@qq.com>
2022-11-03 00:06:53 +0000

    times_test_lch

---
+++ test_times.py
@@ -0,0 +1,16 @@
+from times import compute_overlap_time
+from times import time_range
+import times
+import datetime
+
+def test_given_input():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+
+    result = compute_overlap_time(large, short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+
+    assert result == expected
+
+
+
